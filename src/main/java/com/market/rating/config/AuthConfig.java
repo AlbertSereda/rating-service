52ab55b17fa766b58f2835510abdffdcb15de4f6
Albert Sereda <ASereda@IBS.RU>
2025-02-18 08:35:37 +0500

    Initial commit

---
+++ src/main/java/com/market/rating/config/AuthConfig.java
@@ -0,0 +1,22 @@
+package com.market.category.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AuthConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .oauth2Login(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(r -> r.anyRequest().permitAll())
+                .build();
+    }
+}
